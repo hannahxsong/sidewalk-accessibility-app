cac195a698e857fb1fdb1313a6030a7a44e5688f
ellenwang105 <ellen_wang@horacemann.org>
2026-01-31 13:49:31 -0500

    boston streets

--- src/store.js
+++ src/store.js
@@ -1,14 +1,22 @@
-import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import keplerGlReducer, { enhanceReduxMiddleware } from "@kepler.gl/reducers";
 import { taskMiddleware } from "react-palm/tasks";
 
 const reducers = combineReducers({
-  keplerGl: keplerGlReducer.initialState({
-    uiState: { readOnly: false, currentModal: null },
-  }),
+  keplerGl: keplerGlReducer
 });
 
-// Kepler needs react-palm taskMiddleware + enhanceReduxMiddleware
 const middlewares = enhanceReduxMiddleware([taskMiddleware]);
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-export const store = createStore(reducers, {}, compose(applyMiddleware(...middlewares)));
+export const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+// THIS IS CRITICAL: Force it onto the window immediately
+if (typeof window !== 'undefined') {
+    window.store = store;
+    console.log("Store has been attached to window.store");
+}
